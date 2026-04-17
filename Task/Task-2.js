16a604886fd0376deed413be60b0e9bf56102713
Aniketpatil7278 <142803348+Aniketpatil7278@users.noreply.github.com>
2026-04-17 12:22:35 +0530

    Task-2 Submited

---
+++ Task/Task-2.js
@@ -0,0 +1,13 @@
+// 🔹 Task 2: Let Variable Math
+// Create let marks = 80
+// Add 10 marks and print result.
+
+
+let marks=80;
+
+marks +=10;
+console.log("total Marks: " +marks);
+
+
+//output: total Marks: 90
+ 

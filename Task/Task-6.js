c6b2751bf11f1fd4277bb8c544d3a508d79d283d
Aniketpatil7278 <142803348+Aniketpatil7278@users.noreply.github.com>
2026-04-17 12:52:17 +0530

    Task-6 update

---
+++ Task/Task-6.js
@@ -0,0 +1,15 @@
+// 🔹 Task 6: Array Task
+// Create array with 5 fruits.
+// Print:
+// Full array
+// First fruit
+// Last fruit
+// only banana mongo grapes
+
+
+let fruit =["apple", "banana", "mongo", "grapes", "Orange"];
+
+console.log(fruit); //output:  ['apple', 'banana', 'mongo', 'grapes', 'Orange']
+console.log(fruit[0]);  //output:apple
+console.log(fruit[fruit.length-1]);     //output: Orange
+console.log(fruit.slice(1,4));  //output:  ['banana', 'mongo', 'grapes']

842bc2841d79807fa396e857a1a95c09b82c22fa
Aniketpatil7278 <142803348+Aniketpatil7278@users.noreply.github.com>
2026-04-17 12:35:12 +0530

    Task-4 updated

---
+++ Task/Task-4.js
@@ -0,0 +1,18 @@
+// 🔹 Task 4: Printing Statements
+// Print the following in console:
+// "Welcome Team"
+// 2026
+// true
+
+
+console.log("Welcome Team");
+console.log(2026);
+console.log(true);
+
+
+
+//output:
+
+// Welcome Team
+// Task-4.js:9 2026
+// Task-4.js:10 true

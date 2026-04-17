25cb81d37312e2266a1d312be232c418070c98c3
Aniketpatil7278 <142803348+Aniketpatil7278@users.noreply.github.com>
2026-04-17 12:43:15 +0530

    Task-5 updated

---
+++ Task/Task-5.js
@@ -0,0 +1,19 @@
+// 🔹 Task 5: Datatype Check
+// Check datatype of:
+// "JavaScript"
+// 250
+// false
+// Use typeof
+
+
+console.log(typeof("JavaScript"));
+console.log(typeof(250));
+console.log(typeof(false));
+
+
+//output:
+// string
+// number
+// boolean
+ 
+

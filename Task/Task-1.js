3ab745ef7ae2477417aa91cb310c10e6934afa65
Aniketpatil7278 <142803348+Aniketpatil7278@users.noreply.github.com>
2026-04-17 12:19:32 +0530

    Task-1 Submited

--- Task/Task-1.js
+++ Task/Task-1.js
@@ -1,1 +1,8 @@
-console.log("aniket");
+// Create a variable using var with value 50.
+// Change it to 100 and print final value in console.
+
+
+let a=50;
+a=100;
+console.log("value od a :-" +a);   //output:value od a :-100
+

56071142743e30b5875149a29f0487d73accb94e
Aniketpatil7278 <142803348+Aniketpatil7278@users.noreply.github.com>
2026-04-17 13:16:48 +0530

    Task-11 update

---
+++ Task/Task-11.js
@@ -0,0 +1,12 @@
+// 🔹 Task 11: Logical Operators
+// Print result:
+// 5 > 2 && 10 > 3
+// 7 < 5 || 8 > 2
+// !(10 > 5)
+
+
+console.log(5>2 && 10>3);  //true
+console.log(7<5 || 8>2);  //true
+console.log(!(10>5));   //false
+
+

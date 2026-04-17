cd376e5cf44982055fd4e5e8688f5c369bc989be
Aniketpatil7278 <142803348+Aniketpatil7278@users.noreply.github.com>
2026-04-17 13:20:09 +0530

    Task-12 update

---
+++ Task/Task-12.js
@@ -0,0 +1,17 @@
+// 🔹 Task 12: Ternary Operator
+// Check age:
+// let age = 18;
+// If age >= 18 print "Eligible" else "Not Eligible"
+
+
+let age=prompt();
+
+if (age >=18){
+    console.log("Eligible");
+    alert("Eligible");
+}
+else{
+    console.log("Not Eligible");
+    alert("Not Eligible")
+    
+}

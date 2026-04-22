b09c7c4d23bae6667877c69476882220564a8229
Aniketpatil7278 <142803348+Aniketpatil7278@users.noreply.github.com>
2026-04-22 09:43:08 +0530

    this is the finnal task

--- Task/Task-2.js
+++ Task/Task-2.js
@@ -1,13 +1,1 @@
-// 🔹 Task 2: Let Variable Math
-// Create let marks = 80
-// Add 10 marks and print result.
-
-
-let marks=80;
-
-marks +=10;
-console.log("total Marks: " +marks);
-
-
-//output: total Marks: 90
- 
+// this is the task --2
